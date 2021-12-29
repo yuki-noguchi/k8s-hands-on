e9ef63c124308f03e0b58a9b89eeeccfc9b6a2d2
yuki-noguchi <y.n.10191@gmail.com>
2021-12-29 18:07:49 +0900

    微調整

--- front-app/src/App.svelte
+++ front-app/src/App.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
+  const url = "http://localhost:8080/praise/";
   let mihikoImg = "images/michiko.jpg";
-  let name: string = "";
+  let name = "";
+  let praised = "";
 
-  const handleKeyDown = (event: CustomEvent | KeyboardEvent) => {
-    event = event as KeyboardEvent;
-    if (event.key === "Enter") {
-      // post
+  const postPraise = async () => {
+    if (!name) {
+      return;
+    }
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: name }),
+    });
+    praised = await res.text();
+  };
+
+  const handleKeyPress = async (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      await postPraise();
     }
   };
 
-  const handleSubmit = () => {
-    // post
+  const handleSubmit = async () => {
+    await postPraise();
   };
 </script>
 
 <main>
   <div class="main-container">
+    <h1>みちこがあなたを褒め殺しにするよ！</h1>
     <div class="main-image-container">
       <img src={mihikoImg} alt="みちこさま" />
     </div>
@@ -23,9 +39,15 @@
       <input
         bind:value={name}
         placeholder="名前を入力してね"
-        onkeydown={handleKeyDown}
+        on:keypress={handleKeyPress}
       />
       <button on:click={handleSubmit}>褒められる！</button>
+    </div>
+    <div class="praised">
+      {#if praised}
+        <p>こんなふうに褒めてくれたよ！</p>
+        <p>{praised}</p>
+      {/if}
     </div>
   </div>
 </main>
@@ -40,13 +62,24 @@
   }
 
   .main-image-container {
-    padding-bottom: 20px;
+    padding: 10px;
+  }
+
+  .input-container {
+    padding: 10px;
   }
 
   img {
     border-radius: 50%; /* 角丸半径を50%にする(=円形にする) */
     width: 180px; /* ※縦横を同値に */
     height: 180px; /* ※縦横を同値に */
+  }
+
+  .praised {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
   }
 
   @media (min-width: 640px) {
